0b1e322a2a7d98e379f8879c80e63024b96629d3
codentouge <142188340+newfordmartin@users.noreply.github.com>
2023-09-22 19:11:59 -0400

    Optimize findNeedle() function

--- 3-needle-in-the-haystack.js
+++ 3-needle-in-the-haystack.js
@@ -2,3 +2,7 @@ function findNeedle(haystack) {
   let position = haystack.indexOf("needle");
   return `found the needle at position ${position}`
 }
+
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf('needle')}`;
+}
