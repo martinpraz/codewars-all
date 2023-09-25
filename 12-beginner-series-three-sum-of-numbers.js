8c5ca8aec4299bf6e34398603d692b106fe81429
codentouge <142188340+newfordmartin@users.noreply.github.com>
2023-09-24 21:59:38 -0400

    Rewrote getSum to calculate sum directly in a variable instead of numArray array

--- 12-beginner-series-three-sum-of-numbers.js
+++ 12-beginner-series-three-sum-of-numbers.js
@@ -18,3 +18,24 @@ function getSum(a, b) {
   // Reduce the numArray to a single value and return
   return numArray.reduce((a, b) => a + b);
 }
+
+// #2
+// Remove the unnecessary numArray and directly calculate the sum in a "sum" variable
+function getSum(a, b) {
+  // Check if a === b
+  if (a === b) {
+    return a; // Return either a or b if they are equal.
+  }
+  
+  // Determine the start and finish values
+  let start = Math.min(a, b);
+  let finish = Math.max(a, b);
+
+  // Calculate the sum of integers from start to finish (inclusive)
+  let sum = 0;
+  for (let i = start; i <= finish; i++) {
+    sum += i;
+  }
+  
+  return sum;
+}
