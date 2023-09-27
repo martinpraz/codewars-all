6df93661eaa6f6eedceeab30c0e87671f5ce150f
codentouge <142188340+newfordmartin@users.noreply.github.com>
2023-09-27 13:04:27 -0400

    Add default keyword value for initialVar as function parameter

--- 13-array-sum-odd-even.js
+++ 13-array-sum-odd-even.js
@@ -11,3 +11,10 @@ function oddOrEven(array) {
 function oddOrEven(arr) {
   return arr.reduce((total, currentVal) => total + currentVal, 0) % 2 ? 'odd' : 'even';
 }
+
+function oddOrEven(arr, kwargs = {}) {
+  // Add default value for "initialVal" variable, so that it can be easily passed into the fucntion if need be
+  const {initialVal = 0} = kwargs;
+
+  return arr.reduce((total, currentVal) => total + currentVal, initialVal) % 2 ? 'odd' : 'even';
+}
