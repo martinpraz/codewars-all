4613048ea9973ae90e5898e5ea94dfb7df07c7a4
codentouge <142188340+newfordmartin@users.noreply.github.com>
2023-09-28 11:52:06 -0400

    Minor improvements to handle edge cases more gracefully

--- 16-product-of-maximums-of-array.js
+++ 16-product-of-maximums-of-array.js
@@ -10,3 +10,20 @@ function maxProduct(numbers, size){
   
   return product;
 }
+
+function maxProduct(numbers, size){
+  if (size <= 0 || numbers.length === 0) {
+    return 0; // Return 0 if size is non-positive or the array is empty.
+  }
+
+//   Sort the numbers array in descending order
+  numbers = numbers.sort((a, b) => b - a);
+  let product = 1;
+  
+//   Since the array is already sorted highest values first, you can you iterate over array up to "size" 
+  for (let i = 0; i < size && i < numbers.length; i++) {
+    product *= numbers[i];
+  }
+  
+  return product;
+}
