e7ecfd8c7340eec776b90c1984b1461fa85d12cd
codentouge <142188340+newfordmartin@users.noreply.github.com>
2023-10-08 22:46:49 -0400

    Corrected bug which would consider a number 'jumping' as long as the first 2-digits didn't fail

--- 32-jumping-number.js
+++ 32-jumping-number.js
@@ -9,8 +9,8 @@ function jumpingNumber(n){
   for (let i = 1; i < n.length; i++) {
     if (Math.abs(Number(n[i]) - Number(n[i - 1])) !== 1) {
       return "Not!!";
-    } else {
-      return "Jumping!!";
-    }
+    } 
   }
+  
+  return "Jumping!!";
 }
