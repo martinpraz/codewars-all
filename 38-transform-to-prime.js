13d82b02bc1ea9049623e9be3e9ab8fd09148027
codentouge <142188340+newfordmartin@users.noreply.github.com>
2023-10-18 19:18:17 -0400

    Refactor and encapsulate the code so it's ready for production

--- 38-transform-to-prime.js
+++ 38-transform-to-prime.js
@@ -30,3 +30,48 @@ function minimumNumber(numbers){
 }
   return minNum;
 }
+
+//
+/**
+ * Finds the minimum number of additions required to make the sum of an array of numbers prime.
+ *
+ * @param {number[]} numbers - An array of numbers to be summed.
+ * @returns {number} The minimum number of additions to make the sum prime.
+ */
+function minimumNumber(numbers) {
+  // Calculate the initial sum
+  let base = numbers.reduce((a, b) => a + b, 0);
+
+  // If the initial sum is prime, no additions are needed
+  if (isPrime(base)) {
+    return 0;
+  }
+
+  // Find the minimum number of additions required to make the sum prime
+  let minNum = 0;
+  while (!isPrime(base)) {
+    minNum++;
+    base++;
+  }
+
+  return minNum;
+
+  /**
+   * Checks if a number is prime.
+   *
+   * @param {number} number - The number to be checked for primality.
+   * @returns {boolean} True if the number is prime, false otherwise.
+   */
+  function isPrime(number) {
+    if (number < 2) {
+      return false;
+    } else {
+      for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+}
