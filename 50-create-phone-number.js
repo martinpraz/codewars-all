4d7e60c5411497723c49779a59ea5fada4cd4041
codentouge <142188340+newfordmartin@users.noreply.github.com>
2024-01-19 01:01:31 -0500

    Revert "Add 50-create-phone-number kata"
    This reverts commit 3a2fef4a4d514cc2906815e2197030fad90fcb30.

--- 50-create-phone-number.js
+++
@@ -1,9 +0,0 @@
-
-//
-function createPhoneNumber(numbers){
-  let areaCode = numbers.slice(0, 3).join("");
-  let centralOffice = numbers.slice(3, 6).join("");
-  let lineNumber = numbers.slice(6).join("");
-  
-  return `(${areaCode}) ${centralOffice}-${lineNumber}`
-}
